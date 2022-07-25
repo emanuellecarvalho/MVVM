f44c209ffe7cf12b03129ad1e895c5052e796466
Emanuelle Carvalho <emanuellercm@gmail.com>
2022-07-25 19:45:29 -0300

    config viewModel

---
+++ app/src/main/java/com/emanuellecarvalho/mvvm/viewModel/main/MainViewModel.kt
@@ -0,0 +1,33 @@
+package com.emanuellecarvalho.mvvm.viewModel.main
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.emanuellecarvalho.mvvm.models.Live
+import com.emanuellecarvalho.mvvm.repositories.MainRepository
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+// passing repository on constructor to deal with the data from API
+class MainViewModel(private val repository : MainRepository) : ViewModel() {
+
+    // livedata
+    val liveList = MutableLiveData<List<Live>>()
+    val errorMessage = MutableLiveData<String>()
+
+    fun getAllLives() {
+
+        val request = repository.getAllLives()
+        request.enqueue(object : Callback<List<Live>> {
+            override fun onResponse(call: Call<List<Live>>, response: Response<List<Live>>) {
+                liveList.postValue(response.body())
+            }
+
+            override fun onFailure(call: Call<List<Live>>, t: Throwable) {
+                errorMessage.postValue(t.message)
+            }
+        })
+
+    }
+
+}
