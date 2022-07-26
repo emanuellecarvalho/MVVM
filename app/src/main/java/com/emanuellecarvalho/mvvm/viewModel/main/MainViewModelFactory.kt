da62164b92dee24a5e9098c2d32769626eeb9d80
Emanuelle Carvalho <emanuellercm@gmail.com>
2022-07-26 11:05:08 -0300

    implement ViewModelFactory

---
+++ app/src/main/java/com/emanuellecarvalho/mvvm/viewModel/main/MainViewModelFactory.kt
@@ -0,0 +1,17 @@
+package com.emanuellecarvalho.mvvm.viewModel.main
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.emanuellecarvalho.mvvm.repositories.MainRepository
+
+class MainViewModelFactory(private val repository: MainRepository) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+            MainViewModel(this.repository) as T
+        } else {
+            throw IllegalArgumentException("ViewModel Not Found")
+        }
+    }
+
+}
