50faec7bbac28ddf56bac57d8b166525b87fe471
Emanuelle Carvalho <emanuellercm@gmail.com>
2022-07-26 11:19:54 -0300

    implement RecyclerView on layout

--- app/src/main/java/com/emanuellecarvalho/mvvm/MainActivity.kt
+++ app/src/main/java/com/emanuellecarvalho/mvvm/MainActivity.kt
@@ -2,12 +2,39 @@ package com.emanuellecarvalho.mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.emanuellecarvalho.mvvm.adapters.MainAdapter
+import com.emanuellecarvalho.mvvm.databinding.ActivityMainBinding
+import com.emanuellecarvalho.mvvm.repositories.MainRepository
+import com.emanuellecarvalho.mvvm.rest.RetrofitService
+import com.emanuellecarvalho.mvvm.viewModel.main.MainViewModel
+import com.emanuellecarvalho.mvvm.viewModel.main.MainViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    lateinit var viewModel: MainViewModel
+
+    private val retrofitService = RetrofitService.getInstance()
+
+    private val adapter = MainAdapter {
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        // Criando a viewModel na MainActivity a partir da MainViewModelFactory
+        viewModel =
+            ViewModelProvider(this, MainViewModelFactory(MainRepository(retrofitService))).get(
+                MainViewModel::class.java
+            )
+
+        // Instancindo o adapter
+        binding.recyclerview.adapter = adapter
     }
 }
